2bcf6c716d750dfc4f2a2fae53787aa705a9dc67
knarf7112 <knarf7112@gmail.com>
2015-08-20 18:36:48 +0800

    #0005: 加入Webrequest模組

--- HttpRequest_Test/Program.cs
+++ HttpRequest_Test/Program.cs
@@ -5,22 +5,33 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-//
+//Main1
 using System.Web;
-
+//
+using WebHttpClient;
 namespace HttpRequest_Test
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string uri = "http://10.27.88.164:1539/TxLogFileUpLoadHandler.ashx?%u6A94%u6848=%u6E2C%u8A66%u6A94.txt";
+            string postData = "this is test";
+            byte[] postDataBytes = Encoding.ASCII.GetBytes(postData);
+            byte[] responseData = Client.GetResponse(uri, "Post", postDataBytes);
+            string responseDataStr = Encoding.ASCII.GetString(responseData);
+            Console.WriteLine("回應:" + responseDataStr);
+            Console.ReadKey();
+        }
+        static void Main1(string[] args)
+        {
             HttpRequest rqt1 = new HttpRequest("123.txt", "http://10.27.88.164:1539/TxLogFileUpLoadHandler.ashx", "Test");
-            Uri uri = new Uri("http://10.27.88.164:1539/TxLogFileUpLoadHandler.ashx", UriKind.Absolute);
+            Uri uri = new Uri("http://10.27.88.164:1539/TxLogFileUpLoadHandler.ashx?%u6A94%u6848=%u6E2C%u8A66%u6A94.txt", UriKind.Absolute);
             WebRequest rqt = WebRequest.Create(uri);
             rqt.ContentType = "application/octet-stream";
             ((HttpWebRequest)rqt).UserAgent = ".NET Framework Example Client";
             rqt.Method = "POST";
-
+            
             string postData = "this is test";
             byte[] postDataBytes = Encoding.ASCII.GetBytes(postData);
 
@@ -29,7 +40,7 @@ namespace HttpRequest_Test
             Stream dataStream = rqt.GetRequestStream();
             dataStream.Write(postDataBytes, 0, postDataBytes.Length);
             dataStream.Close();
-
+            
             WebResponse rsp = rqt.GetResponse();
             Console.WriteLine("Response Status:" + ((HttpWebResponse)rsp).StatusDescription);
 
@@ -38,8 +49,9 @@ namespace HttpRequest_Test
             int rspCnt = dataStream.Read(responseBuffer, 0, responseBuffer.Length);
             string responseString = Encoding.ASCII.GetString(responseBuffer, 0, rspCnt);
             Console.WriteLine("Response string:" + responseString);
-
+            
             dataStream.Close();
+            rsp.Close();
             Console.ReadKey();
         }
     }
---
+++ WebHttpClient/Client.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+//
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace WebHttpClient
+{
+    /// <summary>
+    /// 使用WebRequest
+    /// </summary>
+    public class Client
+    {
+        /// <summary>
+        /// 設定Uri,與資料並送出Request後等待Response
+        /// </summary>
+        /// <param name="uriString">目的地Uri</param>
+        /// <param name="method">Get/Post</param>
+        /// <param name="sendData">null(Get)/bytes(Post)</param>
+        /// <param name="debugDisplay"></param>
+        /// <param name="credential">認證用(default:null)</param>
+        /// <param name="timeOut">要求逾時</param>
+        /// <param name="proxyString"></param>
+        /// <returns>回應資料(Byte Array)</returns>
+        public static byte[] GetResponse(string uriString, string method,byte[] sendData,bool debugDisplay = false, ICredentials credential = null, int timeOut = 10000, string proxyString = null)
+        {
+            #region variable
+            WebRequest request = null;
+            Stream dataStream = null;
+            WebResponse response = null;
+            byte[] result = null;
+            Queue<byte> buffer = null;
+            int readByte = -1;
+            #endregion
+
+            request = CreateWebRequest(uriString, timeOut, credential, proxyString);
+
+            SetReuqestData(request, method, sendData);
+
+            ReflectionAllPropertyValue(request);
+            try
+            {
+                response = request.GetResponse();
+                //ReflectionAllPropertyValue(request);
+                dataStream = response.GetResponseStream();
+                buffer = new Queue<byte>();
+
+                while ((readByte = dataStream.ReadByte()) > -1)
+                {
+                    buffer.Enqueue((byte)readByte);
+                }
+                result = buffer.ToArray();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Response讀取異常:" + ex.Message + "\n" + ex.StackTrace);
+            }
+            finally
+            {
+                dataStream.Close();
+                response.Close();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Create WebRequest by UriString and set timeout(ms), credential, proxy
+        /// </summary>
+        /// <param name="uriString">destination Uri</param>
+        /// <param name="timeOut">逾時(ms)</param>
+        /// <param name="credential">Web認證(如果有)</param>
+        /// <param name="proxyString">代理(如果有)</param>
+        /// <returns>WebRequest Object</returns>
+        private static WebRequest CreateWebRequest(string uriString, int timeOut, ICredentials credential, string proxyString)
+        {
+            WebRequest request = null;
+
+            //ref:http://www.dotblogs.com.tw/jaigi/archive/2012/09/29/75169.aspx
+            request = WebRequest.Create(uriString);
+            //time out setting
+            request.Timeout = timeOut;
+            //認證用
+            request.Credentials = credential;//new NetworkCredential("ID","Password","Domain Name") ;
+
+            //proxy setting 
+            if (proxyString != null)
+            {
+                request.Proxy = new WebProxy(proxyString);
+            }
+
+            return request;
+        }
+
+        /// <summary>
+        /// Setting WebRequest Medthod, ContentType, ContentLength, UserAgent, 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="method"></param>
+        /// <param name="sendData"></param>
+        private static void SetReuqestData(WebRequest request,string method,byte[] sendData)
+        {
+            Stream dataStream = null;
+
+            request.Method = method.ToUpper();
+
+            request.ContentLength = (sendData == null) ? 0 : sendData.Length;
+            
+            ((HttpWebRequest)request).UserAgent = "4+ Client";
+
+            
+            switch (method.ToUpper())
+            {
+                case "POST":
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    try
+                    {
+                        dataStream = request.GetRequestStream();
+                        dataStream.Write(sendData, 0, sendData.Length);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Request寫入異常:" + ex.Message + " \n" + ex.StackTrace);
+                    }
+                    finally
+                    {
+                        dataStream.Close();
+                    }
+                    break;
+                case "GET":
+                    request.ContentType = "text/plain";
+                    break;
+                default:
+                    throw new Exception("Method Invalid:" + method);
+            }
+        }
+
+        /// <summary>
+        /// 用來看進入物件的所有有數據的屬性名稱與值
+        /// </summary>
+        /// <param name="obj">Reflected object</param>
+        private static void ReflectionAllPropertyValue(object obj)
+        {
+            try
+            {
+                int count = 0;
+                //get all properties from object
+                System.Reflection.PropertyInfo[] properties = obj.GetType().GetProperties();
+
+                foreach (var property in properties)
+                {
+                    object[] index = null;
+                    //if the property is indexer and has length {ex: System.String [Item]}
+                    //ref:http://stackoverflow.com/questions/6156577/targetparametercountexception-when-enumerating-through-properties-of-string
+                    if (property.GetIndexParameters().Length > 0)
+                    {
+                        index = new object[] { "0" };
+                    }
+                    //get property value and casting to string
+                    string value = property.GetValue(obj, index) as string;
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        //display on debug console
+                        System.Diagnostics.Debug.WriteLine(count++ + ":" + property.Name + ":" + value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Reflection Error:" + ex.StackTrace);
+            }
+        }
+    }
+}
